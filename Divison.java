baaac78311d38de545166341b27465268a6983f0
unknown <rohitsharnagat7@gmail.com>
2022-09-17 15:08:40 +0530

    i am performing operation here

---
+++ Divison.java
@@ -0,0 +1,14 @@
+package com.raps;
+
+public class Divison {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=2;
+		
+		int c=a/b;
+				
+		System.out.println("Divison is >>"+c);
+	}
+
+}
